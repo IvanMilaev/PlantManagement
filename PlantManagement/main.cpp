3548a9ab0946ff54c66ca1fd43eee18d7904cfee
Ivan Milaev <milaevivan@gmail.com>
2019-05-27 22:02:02 +0300

    Initial Commit

---
+++ PlantManagement/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PlantManagement
+//
+//  Created by Иван Милаев on 27/05/2019.
+//  Copyright © 2019 IvanMilaev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
